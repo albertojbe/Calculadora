8d17d614d8cfe78f82ef389ddb44f7dc1f2df554
Alberto Cesar <albertocesar.be@gmail.com>
2024-04-18 21:19:53 -0300

    Iniciando projeto

---
+++ src/test/java/com/example/apiloja/ApiLojaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apiloja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLojaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
